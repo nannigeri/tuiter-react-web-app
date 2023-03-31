7dad9cf456e4d2c42c7df545d3a387d6c3203e3d
nannigeri <annigeri.n@northeastern.edu>
2023-03-31 14:02:22 -0400

    adding changes

--- src/tuiter/tuits/tuits-stats.js
+++ src/tuiter/tuits/tuits-stats.js
@@ -20,27 +20,39 @@ const TuitsStats = (
     }
     return (
         <div className="row mt-2 w-100">
-            <div className="col-3 text-secondary wd-bottom-font-size">
+            <div className="col-2 text-secondary wd-bottom-font-size">
                 <i className="bi bi-chat pe-1"></i>
                 {post.replies}
             </div>
 
-            <div className="col-3 text-secondary wd-bottom-font-size">
+            <div className="col-2 text-secondary wd-bottom-font-size">
                 <i className="bi bi-repeat pe-1"></i>
                 {post.retuits}
             </div>
 
             {post.liked ?
-                <div className="col-3 text-secondary wd-bottom-font-size">
+                <div className="col-2 text-secondary wd-bottom-font-size">
                     <i onClick={()=> dispatch(updateTuitThunk({...post, likes: post.likes -1, liked: false}))} className="bi bi-heart-fill pe-1 wd-red"></i>
                     {post.likes}
                 </div> :
-                <div className="col-3 text-secondary wd-bottom-font-size">
+                <div className="col-2 text-secondary wd-bottom-font-size">
                     <i onClick={()=> dispatch(updateTuitThunk({...post, likes: post.likes +1, liked: true}))} className="bi bi-heart pe-1"></i>
                     {post.likes}
                 </div>
 
             }
+
+                {post.disliked ?
+                        <div className="col-2 text-secondary wd-bottom-font-size">
+                            <i onClick={()=> dispatch(updateTuitThunk({...post, dislikes: post.dislikes -1, disliked: false}))} className="bi bi-hand-thumbs-down pe-1 wd-red"></i>
+                            {post.likes}
+                        </div> :
+                        <div className="col-2 text-secondary wd-bottom-font-size">
+                            <i onClick={()=> dispatch(updateTuitThunk({...post, dislikes: post.dislikes +1, disliked: true}))} className="bi bi-hand-thumbs-down pe-1"></i>
+                            {post.likes}
+                        </div>
+                }
+
 
             <div className="col-3 text-secondary wd-bottom-font-size">
                 <i className="bi bi-upload"></i>
